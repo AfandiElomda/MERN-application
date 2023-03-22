08cb9b29cce74a237ea479ad80d8ee38068727e7
+ <+>
2023-03-22 09:35:10 +0300

    showcase of the piecharts

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import ReactApexChart from 'react-apexcharts';
 import{ Typography,Box,Stack} from '@pankod/refine-mui';
 import { PieChartProps } from 'interfaces/home'
 
@@ -9,6 +9,18 @@ const PieChart = ({ title, value, series, colors}: PieChartProps) => {
         <Typography fontSize={14} color="#808191">{title}</Typography>
         <Typography fontSize={24} color="#11142d" fontWeight={700} mt= {1}>{value}</Typography>
       </Stack>
+
+      <ReactApexChart
+         options={{
+          chart: {type: 'donut'},
+          colors,
+          legend: {show: false},
+          dataLabels: {enabled: false},
+         }}
+         series={series}
+         type="donut"
+         width="120px"
+      />
     </Box>
   )
 }
